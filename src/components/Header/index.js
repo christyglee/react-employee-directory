5cc1fb946def867f4ed97ad143b56ada70367c67
unknown <christy.g.lee@gmail.com>
2020-11-14 02:37:55 -0800

    updated heading p tag

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header(props) {
     return (
         <div className="header">
             <h1>Employee Directory</h1>
-            <p>Use the search box to narrow your results or click on carrots to filter</p>
+            <p>Use the search box to narrow or filter your results or click on a table header to sort</p>
         </div>
     );
 }
